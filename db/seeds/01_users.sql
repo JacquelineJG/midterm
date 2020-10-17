6f7114a5a907e0a21ada2e1d299737b8931dd4c0
Sean Kim <sean.sihun.kim@gmail.com>
2020-10-17 00:31:59 +0000

    fix

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (name, email, password)
-VALUES ('Chris, csjohn1@gmail.com', 'password'),('JJ', 'JJ@gmail.com','hamilton'), ('Skim', 'skim@gmail.com', 'korea');
+VALUES ('Chris', 'csjohn1@gmail.com', 'password'),('JJ', 'JJ@gmail.com','hamilton'), ('Skim', 'skim@gmail.com', 'korea');
